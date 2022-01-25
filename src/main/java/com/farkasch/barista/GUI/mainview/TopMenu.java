03c6875778f9325a7eaef9685742e96296102730
FarkaschZoltan <farkasch.zoltan@gmail.com>
2022-01-25 23:46:13 +0100

    Added a temporary "New File" option, which creates a dummy file for testing

--- src/main/java/com/farkasch/barista/GUI/mainview/TopMenu.java
+++ src/main/java/com/farkasch/barista/GUI/mainview/TopMenu.java
@@ -1,17 +1,25 @@
-package com.farkasch.barista.GUI.menu;
+package com.farkasch.barista.GUI.mainview;
 
+import com.farkasch.barista.MainApp;
+import java.io.File;
+import java.io.IOException;
 import javafx.scene.control.Menu;
 import javafx.scene.control.MenuBar;
 import javafx.scene.control.MenuItem;
 
+
 public class TopMenu extends MenuBar {
+
+    private MainApp mainApp;
+
     private Menu fileMenu;
     private Menu settingsMenu;
     private Menu gitMenu;
     private Menu helpMenu;
 
+    public TopMenu(MainApp mainApp){
+        this.mainApp = mainApp;
 
-    public TopMenu(){
         initFileMenu();
         initSettingsMenu();
         initGitMenu();
@@ -23,11 +31,24 @@ public class TopMenu extends MenuBar {
     private void initFileMenu(){
         fileMenu = new Menu("File");
 
+        MenuItem newFile = new MenuItem("New File");
+        newFile.setOnAction(action -> {
+            File f = new File("C:\\Users\\farka\\Documents\\teszt\\teszt" + (int)(Math.random() * (100 - 0 + 1) + 0) + ".txt");
+            if(!f.exists()){
+                try{
+                    f.createNewFile();
+                } catch(IOException e){
+                    System.out.println("Error while creating file!");
+                    e.printStackTrace();
+                }
+            }
+            mainApp.getCodeArea().openFile(0, f);
+        });
         MenuItem newProject = new MenuItem("New Project");
         MenuItem loadProject = new MenuItem("Load Project");
         MenuItem saveProject = new MenuItem("Save");
 
-        fileMenu.getItems().addAll(newProject, loadProject, saveProject);
+        fileMenu.getItems().addAll(newFile, newProject, loadProject, saveProject);
     }
 
     private void initGitMenu(){
