d3e182966cc8c85cdd7fa3260ee94be15609dd20
FarkaschZoltan <farkasch.zoltan@gmail.com>
2022-02-07 20:18:00 +0100

    CodingInterface now uses Hack-Bold.ttf font

--- src/main/java/com/farkasch/barista/gui/codinginterface/CodingInterface.java
+++ src/main/java/com/farkasch/barista/gui/codinginterface/CodingInterface.java
@@ -3,9 +3,11 @@ package com.farkasch.barista.gui.codinginterface;
 import com.farkasch.barista.services.FileService;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.nio.file.Paths;
 import java.util.Scanner;
 import javafx.scene.control.TextArea;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.text.Font;
 
 public class CodingInterface extends BorderPane {
 
@@ -23,6 +25,10 @@ public class CodingInterface extends BorderPane {
 
     public CodingInterface(CodingInterfaceContainer parent) {
         content = new TextArea();
+        content.setFont(Font.loadFont(getClass().getResource(
+                "/fonts/Hack-Bold.ttf").toExternalForm(),
+            12));
+
         switchMenu = new SwitchMenu(this);
         this.parent = parent;
 
@@ -56,7 +62,7 @@ public class CodingInterface extends BorderPane {
         }
     }
 
-    public void close(){
+    public void close() {
         parent.closeInterface(this);
     }
 }
