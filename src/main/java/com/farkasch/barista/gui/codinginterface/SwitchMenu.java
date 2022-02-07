98523a624791e76d54f462532113ce563764b85c
FarkaschZoltan <farkasch.zoltan@gmail.com>
2022-02-07 20:17:03 +0100

    SwitchMenuItems visual change update

--- src/main/java/com/farkasch/barista/gui/codinginterface/SwitchMenu.java
+++ src/main/java/com/farkasch/barista/gui/codinginterface/SwitchMenu.java
@@ -1,8 +1,6 @@
 package com.farkasch.barista.gui.codinginterface;
 
 import java.io.File;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.scene.Node;
 import javafx.scene.control.Button;
 import javafx.scene.layout.HBox;
@@ -31,6 +29,9 @@ public class SwitchMenu extends HBox {
     public void addFile(File file) {
         SwitchMenuItem widget = new SwitchMenuItem(file);
         getChildren().add(widget);
+        if (currentlyActive != null) {
+            currentlyActive.setContentId("switch-menu__item");
+        }
         currentlyActive = widget;
     }
 
@@ -61,22 +62,29 @@ public class SwitchMenu extends HBox {
             this.file = file;
         }
 
+        public void setContentId(String id) {
+            openButton.setId(id);
+            closeButton.setId(id);
+        }
+
         public SwitchMenuItem(File file) {
             this.file = file;
+            setFillHeight(true);
             initOpenButton();
             initCloseButton();
-            setId("switch-menu__item--selected");
         }
 
         private void initOpenButton() {
             openButton = new Button(file.getName());
             openButton.setOnAction(actionEvent -> {
                 codingInterface.showFile(file);
-                currentlyActive.setId("switch-menu__item");
+                currentlyActive.setContentId("switch-menu__item");
                 currentlyActive = this;
-                currentlyActive.setId("switch-menu__item--selected");
+                currentlyActive.setContentId("switch-menu__item--selected");
             });
             getChildren().add(openButton);
+            openButton.setMinHeight(getHeight());
+            openButton.setId("switch-menu__item--selected");
         }
 
         private void initCloseButton() {
@@ -91,13 +99,20 @@ public class SwitchMenu extends HBox {
                     codingInterface.showFile(
                         ((SwitchMenuItem) menu.getChildren().get(index - 1)).getFile());
                     menu.removeFile(index);
+                    currentlyActive = (SwitchMenuItem) menu.getChildren().get(index - 1);
+                    currentlyActive.setContentId("switch-menu__item--selected");
                 } else {
-                    codingInterface.showFile(((SwitchMenuItem)menu.getChildren().get(index + 1)).getFile());
+                    codingInterface.showFile(
+                        ((SwitchMenuItem) menu.getChildren().get(index + 1)).getFile());
                     menu.removeFile(index);
+                    currentlyActive = (SwitchMenuItem) menu.getChildren().get(index + 1);
+                    currentlyActive.setContentId("switch-menu__item--selected");
                 }
             });
             closeButton.setGraphic(new FontIcon("mdi-close"));
+            closeButton.setMinHeight(getHeight());
             getChildren().add(closeButton);
+            closeButton.setId("switch-menu__item--selected");
         }
 
     }
