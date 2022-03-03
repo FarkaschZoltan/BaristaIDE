15270f4f8a5382b1efb054bfb50c4eb3c732e9ca
FarkaschZoltan <farkasch.zoltan@gmail.com>
2022-03-03 11:41:31 +0100

    files/folder with spaces are now supported

--- src/main/java/com/farkasch/barista/gui/mainview/topmenu/OpenFileWindow.java
+++ src/main/java/com/farkasch/barista/gui/mainview/topmenu/OpenFileWindow.java
@@ -1,6 +1,7 @@
 package com.farkasch.barista.gui.mainview.topmenu;
 
 import com.farkasch.barista.services.ProcessService;
+import com.sun.javafx.scene.control.LabeledText;
 import java.io.File;
 import java.nio.file.Paths;
 import java.util.List;
@@ -16,7 +17,6 @@ import javafx.scene.control.ScrollPane.ScrollBarPolicy;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
-import javafx.scene.layout.Priority;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 import javafx.util.Pair;
@@ -25,125 +25,126 @@ import org.springframework.lang.Nullable;
 
 public class OpenFileWindow extends Stage {
 
-    private Label fileName;
-    private Label fileNameLabel;
-    private Label chooseFileLabel;
-    private Button openFileButton;
-    private GridPane fileNameLayout;
-    private GridPane rootFolderSelector;
-    private ScrollPane scrollPane;
-    private VBox windowLayout;
-    private HBox openButtonContainer;
-    private Scene scene;
+  private Label fileName;
+  private Label fileNameLabel;
+  private Label chooseFileLabel;
+  private Button openFileButton;
+  private GridPane fileNameLayout;
+  private GridPane rootFolderSelector;
+  private ScrollPane scrollPane;
+  private VBox windowLayout;
+  private HBox openButtonContainer;
+  private Scene scene;
 
-    private String filePath;
+  private String filePath;
 
-    public OpenFileWindow(Consumer<File> openFile) {
-        fileName = new Label("");
-        fileNameLabel = new Label("Chosen File: ");
-        chooseFileLabel = new Label("Choose a file: ");
+  public OpenFileWindow(Consumer<File> openFile) {
+    fileName = new Label("");
+    fileNameLabel = new Label("Chosen File: ");
+    chooseFileLabel = new Label("Choose a file: ");
 
-        openFileButton = new Button("Open");
+    openFileButton = new Button("Open");
 
-        rootFolderSelector = new GridPane();
-        fileNameLayout = new GridPane();
+    rootFolderSelector = new GridPane();
+    fileNameLayout = new GridPane();
 
-        scrollPane = new ScrollPane(rootFolderSelector);
-        openButtonContainer = new HBox(openFileButton);
-        windowLayout = new VBox(fileNameLayout, scrollPane, openButtonContainer);
+    scrollPane = new ScrollPane(rootFolderSelector);
+    openButtonContainer = new HBox(openFileButton);
+    windowLayout = new VBox(fileNameLayout, scrollPane, openButtonContainer);
 
-        scene = new Scene(windowLayout, 300, 400);
+    scene = new Scene(windowLayout, 300, 400);
 
-        filePath = "";
+    filePath = "";
 
-        init(openFile);
+    init(openFile);
+  }
+
+  private void init(Consumer<File> openFile) {
+    setTitle("Open File");
+
+    scene.getStylesheets().add(
+      Paths.get("src/main/java/com/farkasch/barista/style.css").toAbsolutePath().toUri()
+        .toString());
+
+    scrollPane.setHbarPolicy(ScrollBarPolicy.AS_NEEDED);
+    scrollPane.setVbarPolicy(ScrollBarPolicy.AS_NEEDED);
+    scrollPane.setPrefHeight(scene.getHeight());
+    VBox.setMargin(scrollPane, new Insets(10));
+
+    fileNameLabel.setLabelFor(fileName);
+
+    fileNameLayout.add(fileNameLabel, 0, 0);
+    GridPane.setMargin(fileNameLabel, new Insets(10, 20, 10, 10));
+    GridPane.setValignment(fileNameLabel, VPos.CENTER);
+    fileNameLayout.add(fileName, 1, 0);
+    fileNameLayout.add(chooseFileLabel, 0, 1);
+    GridPane.setMargin(chooseFileLabel, new Insets(10, 0, 0, 10));
+
+    openFileButton.setOnAction(actionEvent -> {
+      File file = new File(filePath);
+      openFile.accept(file);
+      close();
+    });
+
+    openButtonContainer.setAlignment(Pos.BOTTOM_RIGHT);
+    VBox.setMargin(openButtonContainer, new Insets(10));
+
+    folderExpand(null, null);
+    setScene(scene);
+  }
+
+  private void folderExpand(@Nullable String parentName, @Nullable VBox parentContainer) {
+    List<Pair<String, Boolean>> dirs = ProcessService.getDirsAndFiles(parentName);
+    GridPane folderSelector = null;
+    if (parentContainer == null) {
+      folderSelector = rootFolderSelector;
+    } else {
+      folderSelector = new GridPane();
     }
 
-    private void init(Consumer<File> openFile) {
-        setTitle("Open File");
-
-        scene.getStylesheets().add(
-            Paths.get("src/main/java/com/farkasch/barista/style.css").toAbsolutePath().toUri()
-                .toString());
-
-        scrollPane.setHbarPolicy(ScrollBarPolicy.AS_NEEDED);
-        scrollPane.setVbarPolicy(ScrollBarPolicy.AS_NEEDED);
-        scrollPane.setPrefHeight(scene.getHeight());
-        VBox.setMargin(scrollPane, new Insets(10));
-
-        fileNameLabel.setLabelFor(fileName);
-
-        fileNameLayout.add(fileNameLabel, 0, 0);
-        GridPane.setMargin(fileNameLabel, new Insets(10, 20, 10, 10));
-        GridPane.setValignment(fileNameLabel, VPos.CENTER);
-        fileNameLayout.add(fileName, 1, 0);
-        fileNameLayout.add(chooseFileLabel, 0, 1);
-        GridPane.setMargin(chooseFileLabel, new Insets(10, 0, 0, 10));
-
-        openFileButton.setOnAction(actionEvent -> {
-            File file = new File(filePath);
-            openFile.accept(file);
-            close();
-        });
-
-        openButtonContainer.setAlignment(Pos.BOTTOM_RIGHT);
-        VBox.setMargin(openButtonContainer, new Insets(10));
-
-        folderExpand(null, null);
-        setScene(scene);
-    }
-
-    private void folderExpand(@Nullable String parentName, @Nullable VBox parentContainer) {
-        List<Pair<String, Boolean>> dirs = ProcessService.getDirsAndFiles(parentName);
-        GridPane folderSelector = null;
-        if (parentContainer == null) {
-            folderSelector = rootFolderSelector;
+    for (int i = 0; i < dirs.size(); i++) {
+      VBox folderContainer = new VBox();
+      folderContainer.setMinWidth(
+        parentContainer == null ? scene.getWidth() : parentContainer.getWidth());
+      Button folderButton = new Button(dirs.get(i).getKey());
+      Boolean isFile = dirs.get(i).getValue();
+      if (isFile) {
+        folderButton.setGraphic(new FontIcon("mdi-file"));
+      } else {
+        folderButton.setGraphic(new FontIcon("mdi-folder"));
+      }
+      folderButton.addEventHandler(MouseEvent.MOUSE_CLICKED, mouseEvent -> {
+        Button target = folderButton;
+        VBox parent = (VBox) (target.getParent());
+        if (parent.getChildren().size() > 1) {
+          folderClose(parent);
+        } else if (!isFile.booleanValue()) {
+          folderExpand((parentName == null ? "" : parentName) + "\\" + target.getText(),
+            folderContainer);
         } else {
-            folderSelector = new GridPane();
+          fileName.setText(target.getText());
+          filePath = "C:\\Users\\" + parentName + "\\" + target.getText();
         }
+      });
 
-        for (int i = 0; i < dirs.size(); i++) {
-            VBox folderContainer = new VBox();
-            folderContainer.setMinWidth(
-                parentContainer == null ? scene.getWidth() : parentContainer.getWidth());
-            Label folderLabel = new Label(dirs.get(i).getKey());
-            Boolean isFile = dirs.get(i).getValue();
-            if (isFile) {
-                folderLabel.setGraphic(new FontIcon("mdi-file"));
-            } else {
-                folderLabel.setGraphic(new FontIcon("mdi-folder"));
-            }
-            folderLabel.addEventHandler(MouseEvent.MOUSE_CLICKED, mouseEvent -> {
-                Label target = ((Label) mouseEvent.getTarget());
-                VBox parent = (VBox) (target.getParent());
-                if (parent.getChildren().size() > 1) {
-                    folderClose(parent);
-                } else if (!isFile.booleanValue()) {
-                    folderExpand((parentName == null ? "" : parentName) + "\\" + target.getText(),
-                        folderContainer);
-                } else {
-                    fileName.setText(target.getText());
-                    filePath = "C:\\Users\\" + parentName + "\\" + target.getText();
-                }
-            });
-            folderLabel.setId("folder");
-            folderLabel.setMaxWidth(Double.MAX_VALUE);
-            folderLabel.setMaxHeight(Double.MAX_VALUE);
-            folderContainer.getChildren().add(folderLabel);
-            folderSelector.addRow(i, folderContainer);
-            if (parentContainer != null) {
-                folderSelector.setPadding(new Insets(0, 0, 0, 20));
-            }
-            //folderSelector.setGridLinesVisible(true);
-        }
-
-        if (parentContainer != null) {
-            parentContainer.getChildren().add(folderSelector);
-        }
+      folderButton.setId("folder");
+      folderButton.setMaxWidth(Double.MAX_VALUE);
+      folderButton.setMaxHeight(Double.MAX_VALUE);
+      folderContainer.getChildren().add(folderButton);
+      folderSelector.addRow(i, folderContainer);
+      if (parentContainer != null) {
+        folderSelector.setPadding(new Insets(0, 0, 0, 20));
+      }
+      //folderSelector.setGridLinesVisible(true);
     }
 
-    private void folderClose(VBox parent) {
-        System.out.println(parent);
-        parent.getChildren().remove(1, parent.getChildren().size());
+    if (parentContainer != null) {
+      parentContainer.getChildren().add(folderSelector);
     }
+  }
+
+  private void folderClose(VBox parent) {
+    System.out.println(parent);
+    parent.getChildren().remove(1, parent.getChildren().size());
+  }
 }
--- src/main/java/com/farkasch/barista/services/ProcessService.java
+++ src/main/java/com/farkasch/barista/services/ProcessService.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -12,75 +13,86 @@ import org.springframework.lang.Nullable;
 
 public class ProcessService {
 
-    public static List<String> getDirs(@Nullable String folder) {
-        List<String> dirs = new ArrayList<>();
+  public static List<String> getDirs(@Nullable String folder) {
+    List<String> dirs = new ArrayList<>();
 
-        ProcessBuilder pb = new ProcessBuilder();
-        if (folder == null) {
-            pb.directory(new File("C:\\Users"));
-        } else {
-            pb.directory(new File("C:\\Users" + folder));
+    ProcessBuilder pb = new ProcessBuilder();
+    if (folder == null) {
+      pb.directory(new File("C:\\Users"));
+    } else {
+      pb.directory(new File("C:\\Users" + folder));
+    }
+    pb.command("CMD", "/C", "dir");
+
+    try {
+      Process process = pb.start();
+      BufferedReader reader = new BufferedReader(
+        new InputStreamReader(process.getInputStream()));
+      String line;
+
+      while ((line = reader.readLine()) != null) {
+        //System.out.println(line);
+        List<String> splitLine = Arrays.asList(line.split(" "));
+        if (splitLine.contains("<DIR>") && !(splitLine.contains(".") || splitLine.contains(
+          ".."))) {
+          dirs.add(splitLine.get(splitLine.size() - 1));
         }
-        pb.command("CMD", "/C", "dir");
+      }
 
-        try {
-            Process process = pb.start();
-            BufferedReader reader = new BufferedReader(
-                new InputStreamReader(process.getInputStream()));
-            String line;
-
-            while ((line = reader.readLine()) != null) {
-                //System.out.println(line);
-                List<String> splitLine = Arrays.asList(line.split(" "));
-                if (splitLine.contains("<DIR>") && !(splitLine.contains(".") || splitLine.contains(
-                    ".."))) {
-                    dirs.add(splitLine.get(splitLine.size() - 1));
-                }
-            }
-
-            //dirs.stream().forEach(s -> System.out.println("-> " + s));
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        return dirs;
+      //dirs.stream().forEach(s -> System.out.println("-> " + s));
+    } catch (IOException e) {
+      e.printStackTrace();
     }
 
-    public static List<Pair<String, Boolean>> getDirsAndFiles(@Nullable String folder){
-        List<Pair<String, Boolean>> dirsAndFiles = new ArrayList<>();
+    return dirs;
+  }
 
-        ProcessBuilder pb = new ProcessBuilder();
-        if (folder == null) {
-            pb.directory(new File("C:\\Users"));
-        } else {
-            pb.directory(new File("C:\\Users" + folder));
-        }
-        pb.command("CMD", "/C", "dir");
+  public static List<Pair<String, Boolean>> getDirsAndFiles(@Nullable String folder) {
+    List<Pair<String, Boolean>> dirsAndFiles = new ArrayList<>();
 
-        try {
-            Process process = pb.start();
-            BufferedReader reader = new BufferedReader(
-                new InputStreamReader(process.getInputStream()));
-            String line;
-
-            while ((line = reader.readLine()) != null) {
-                System.out.println(line);
-                List<String> splitLine = Arrays.asList(line.split(" "));
-                if (splitLine.contains("<DIR>") && !(splitLine.contains(".") || splitLine.contains(
-                    ".."))) {
-                    dirsAndFiles.add(new Pair<String, Boolean>(splitLine.get(splitLine.size() - 1), false));
-                }
-                else if(splitLine.get(splitLine.size()-1).matches(".*[.].[a-z0-9]{2,3}")){
-                    dirsAndFiles.add(new Pair<String, Boolean>(splitLine.get(splitLine.size() - 1), true));
-                }
-            }
-
-            dirsAndFiles.stream().forEach(s -> System.out.println("-> " + s));
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        return dirsAndFiles;
+    ProcessBuilder pb = new ProcessBuilder();
+    if (folder == null) {
+      pb.directory(new File("C:\\Users"));
+    } else {
+      pb.directory(new File("C:\\Users" + folder));
     }
+    pb.command("CMD", "/C", "dir");
+
+    try {
+      Process process = pb.start();
+      BufferedReader reader = new BufferedReader(
+        new InputStreamReader(process.getInputStream()));
+      String line;
+
+      while ((line = reader.readLine()) != null) {
+        System.out.println(line);
+        List<String> splitLine = Arrays.asList(line.split(" "));
+        if (splitLine.contains("<DIR>") && !(splitLine.contains(".") || splitLine.contains(
+          ".."))) {
+          splitLine = splitLine.stream().filter(s -> !s.equals("")).toList();
+          int startIndex = splitLine.indexOf("<DIR>") + 1;
+          String folderName = splitLine.get(startIndex);
+          for(int i = startIndex + 1;i < splitLine.size(); i++){
+            folderName += " " + splitLine.get(i);
+          }
+          dirsAndFiles.add(new Pair<>(folderName, false));
+        } else if (splitLine.get(splitLine.size() - 1).matches(".*[.].[a-z0-9]{2,3}")) {
+          splitLine = splitLine.stream().filter(s -> !s.equals("")).toList();
+          int startIndex = 5;
+          String fileName = splitLine.get(startIndex);
+          for(int i = startIndex + 1;i < splitLine.size(); i++){
+            fileName += " " + splitLine.get(i);
+          }
+          dirsAndFiles.add(new Pair<>(fileName, true));
+        }
+      }
+
+      dirsAndFiles.stream().forEach(s -> System.out.println("-> " + s));
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+
+    return dirsAndFiles;
+  }
 
 }
